cb888e1b7c2cb175ec9afd92260cad23295ea1b3
jhumaira <141977921+jhumaira@users.noreply.github.com>
2025-08-05 01:05:09 -0500

    Updates

--- src/pages/FAQ.jsx
+++
@@ -1,9 +0,0 @@
-import FAQSection from "../components/FAQSection";
-
-export default function FAQ() {
-  return (
-    <div className="bg-white min-h-screen">
-      <FAQSection />
-    </div>
-  );
-}
